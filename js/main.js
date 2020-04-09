1c7cd5245d5f6b15c21481917d115a9601889bee
Street-Sport <52323611+Street-Sport@users.noreply.github.com>
2020-04-10 00:23:10 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -73,16 +73,7 @@ $(function(){
     
   responsive: [
     {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
+      breakpoint: 640,
       settings: {
         arrows: false,
         centerMode: true,
